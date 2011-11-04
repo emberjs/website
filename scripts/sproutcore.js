aeb52264b06d843a2cfcad627215d238aaad317b
Peter Wagenet <peter.wagenet@gmail.com>
2011-11-04 12:10:46 -0700

    Updated SproutCore

--- scripts/sproutcore.js
+++ scripts/sproutcore.js
@@ -13557,13 +13557,6 @@ SC._BindableSpanView = SC.View.extend(SC.Metamorph,
     }
 
     return this._super(buffer);
-  },
-
-  destroy: function() {
-    var removeObserver = get(this, 'removeObserver');
-    removeObserver();
-
-    this._super();
   }
 });
 
@@ -13613,15 +13606,15 @@ var get = SC.get, getPath = SC.getPath, set = SC.set, fmt = SC.String.fmt;
         });
       };
 
-      set(bindView, 'removeObserver', function() {
-        SC.removeObserver(ctx, property, observer);
-      });
-
       // Observes the given property on the context and
       // tells the SC._BindableSpan to re-render. If property
       // is an empty string, we are printing the current context
       // object ({{this}}) so updating it is not our responsibility.
       if (property !== '') {
+        set(bindView, 'removeObserver', function() {
+          SC.removeObserver(ctx, property, observer);
+        });
+
         SC.addObserver(ctx, property, observer);
       }
     } else {
@@ -14022,7 +14015,11 @@ SC.Handlebars.ViewHelper = SC.Object.create({
           if (PARENT_VIEW_PATH.test(path)) {
             SC.Logger.warn("As of SproutCore 2.0 beta 3, it is no longer necessary to bind to parentViews. Instead, please provide binding paths relative to the current Handlebars context.");
           } else {
-            options[prop] = 'bindingContext.'+path;
+            if (path === 'this') {
+              options[prop] = 'bindingContext';
+            } else {
+              options[prop] = 'bindingContext.'+path;
+            }
           }
         }
       }
@@ -14256,6 +14253,46 @@ SC.Handlebars.registerHelper('each', function(path, options) {
   options.hash.contentBinding = path;
   options.hash.preserveContext = true;
   return SC.Handlebars.helpers.collection.call(this, 'SC.Handlebars.EachView', options);
+});
+
+})({});
+
+
+(function(exports) {
+/**
+  `template` allows you to render a template from inside another template.
+  This allows you to re-use the same template in multiple places. For example:
+
+      <script type="text/x-handlebars">
+        {{#with loggedInUser}}
+          Last Login: {{lastLogin}}
+          User Info: {{template "user_info"}}
+        {{/with}}
+      </script>
+
+      <script type="text/x-handlebars" data-template-name="user_info">
+        Name: <em>{{name}}</em>
+        Karma: <em>{{karma}}</em>
+      </script>
+
+  This helper looks for templates in the global SC.TEMPLATES hash. If you
+  add <script> tags to your page with the `data-template-name` attribute set,
+  they will be compiled and placed in this hash automatically.
+
+  You can also manually register templates by adding them to the hash:
+
+      SC.TEMPLATES["my_cool_template"] = SC.Handlebars.compile('<b>{{user}}</b>');
+
+  @name Handlebars.helpers.template
+  @param {String} templateName the template to render
+*/
+
+SC.Handlebars.registerHelper('template', function(name, options) {
+  var template = SC.TEMPLATES[name];
+
+  sc_assert("Unable to find template with name '"+name+"'.", !!template);
+
+  SC.TEMPLATES[name](this, { data: options.data });
 });
 
 })({});
