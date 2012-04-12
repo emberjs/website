0338e0aee797e661804a5e41ea27eeca8863d554
tomhuda <tomhuda@tilde.io>
2012-04-12 13:59:28 -0700

    documentation improvements

--- config.rb
+++ config.rb
@@ -38,35 +38,17 @@ class TableOfContents < Redcarpet::Render::Base
   end
 
   def header(text, level)
-    return if level > 3
+    @toc_count += 1
 
-    result = @result
-
-    if level > @current_level
-      while level > @current_level
-        result << "<ul>\n<li>\n"
-        @current_level += 1
-      end
-    elsif level < @current_level
-      result << "</li>\n"
-
-      while level < @current_level
-        result << "</ul>\n</li>\n"
-        @current_level -= 1
-      end
-
-      result << "<li>\n"
-    else
-      result << "</li>\n<li>\n"
-    end
-
-    result << "<a href=\"#toc_#{TableOfContents.anchorify(text)}\">#{text}</a>"
+    return "" if level > 3
+    link = "<a href=\"#toc_#{@toc_count-1}\">#{text}</a>"
+    @result << %Q{<li class="level-#{level}">#{link}</li>}
 
     ""
   end
 
   def postprocess(text)
-    @result.join("")
+    "<ol>" + @result.join("") + "</ol>"
   end
 end
 
@@ -156,7 +138,7 @@ set :md, :layout_engine => :erb,
 
 activate :directory_indexes
 
-page "documentation.html" do
+page "/doc*" do
   @chapters = data.docs.chapters
 end
 
