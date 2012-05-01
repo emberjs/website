4cede3376aa59d2a57a7be10259ec8291d64b846
tomhuda <tomhuda@tilde.io>
2012-04-30 17:45:29 -0700

    Add an edge template

---
+++ source/edge_template.rb
@@ -0,0 +1,32 @@
+# This script is designed to be used with Ruby on Rails' new project generator:
+#
+#     rails new my_app -m http://emberjs.com/template.rb
+#
+# For more information about the template API, please see the following Rails
+# guide:
+#
+#     http://edgeguides.rubyonrails.org/rails_application_templates.html
+
+run "rm public/index.html"
+
+# Install required gems
+gem "active_model_serializers"
+gem_group :assets do
+  gem "ember-rails", github: "emberjs/ember-rails"
+end
+
+run "bundle install"
+
+# Configure the app to serve Ember.js and app assets from an AssetsController
+generate "ember:bootstrap"
+generate "ember:install", "--head"
+generate :controller, "Assets", "index"
+
+route "root :to => 'assets#index'"
+
+# Generate a default serializer that is compatible with ember-data
+generate :serializer, "application", "--parent", "ActiveModel::Serializer"
+inject_into_class "app/serializers/application_serializer.rb", 'ApplicationSerializer' do
+  "  embed :ids, :include => true\n"
+end
+
