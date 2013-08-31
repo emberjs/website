d687e24fabd4b76942b5de92ad61b48159619a5e
Tom Dale <tom@tomdale.net>
2013-08-31 10:31:18 -0700

    Example styling

--- source/examples/components/components/gravatar.js
+++ source/examples/components/components/gravatar.js
@@ -1,11 +1,10 @@
 App.GravatarImageComponent = Ember.Component.extend({
   size: 200,
+  email: '',
 
   gravatarUrl: function() {
     var email = this.get('email'),
         size = this.get('size');
-
-    if (!email) { return; }
 
     return "http://www.gravatar.com/avatar/" + hex_md5(email) + '?s=' + size;
   }.property('email', 'size')
