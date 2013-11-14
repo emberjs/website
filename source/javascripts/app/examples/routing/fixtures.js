94a7101f998f88ea5c76a08f50e85cb939d4298f
Franck Verrot <franck@verrot.fr>
2013-11-14 23:40:09 +0100

    Spam can come from anywhere
    This commit removes the reference to Nigeria in the mailbox example.

--- source/javascripts/app/examples/routing/fixtures.js
+++ source/javascripts/app/examples/routing/fixtures.js
@@ -25,8 +25,8 @@ App.FIXTURES = [
     messages: [
       {
         id: 3,
-        subject: "You have one the Nigerian lottery!!!111ONEONE",
-        from: "419@thereallotteryhonest.ng",
+        subject: "You have one the lottery!!!111ONEONE",
+        from: "419@thereallotteryhonest.com",
         to: "user@example.com",
         date: new Date(),
         body: "You have ONE the lottery! You only have to send us a small amount of monies to claim your prize"
