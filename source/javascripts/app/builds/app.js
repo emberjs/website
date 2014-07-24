1beff68ef423c3b0673a345747539d3d43e5d27e
Robert Jackson <robert.w.jackson@me.com>
2014-07-24 00:33:11 -0400

    Update fixture data for 1.7.0-beta.3

--- source/javascripts/app/builds/app.js
+++ source/javascripts/app/builds/app.js
@@ -195,13 +195,13 @@ App.Project.reopenClass({
       projectName: "Ember",
       projectFilter: "ember",
       projectRepo: 'emberjs/ember.js',
-      lastRelease: "1.7.0-beta.2",
-      futureVersion: "1.7.0-beta.3",
+      lastRelease: "1.7.0-beta.3",
+      futureVersion: "1.7.0-beta.4",
       finalVersion: '1.7.0',
       channel: "beta",
       cycleEstimatedFinishDate: '2014-08-19',
-      date: "2014-07-15",
-      nextDate: "2014-07-22",
+      date: "2014-07-24",
+      nextDate: "2014-07-30",
       changelogPath: "CHANGELOG.md"
     }, {
       projectName: "Ember Data",
