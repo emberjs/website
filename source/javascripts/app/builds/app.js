e4f4e04ab092f552f5a960bc29cf26d082e761c7
Robert Jackson <robert.w.jackson@me.com>
2014-10-13 17:30:25 -0400

    Update fixture data for 1.8.0-beta.5.

--- source/javascripts/app/builds/app.js
+++ source/javascripts/app/builds/app.js
@@ -195,13 +195,13 @@ App.Project.reopenClass({
       projectName: "Ember",
       projectFilter: "ember",
       projectRepo: 'emberjs/ember.js',
-      lastRelease: "1.8.0-beta.4",
-      futureVersion: "1.8.0-beta.5",
+      lastRelease: "1.8.0-beta.5",
+      futureVersion: "1.8.0",
       finalVersion: '1.8.0',
       channel: "beta",
       cycleEstimatedFinishDate: '2014-10-24',
-      date: "2014-10-05",
-      nextDate: "2014-10-10",
+      date: "2014-10-13",
+      nextDate: "2014-10-24",
       changelogPath: "CHANGELOG.md"
     }, {
       projectName: "Ember Data",
