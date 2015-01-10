fef726a617bc62cea20efbee8d110a3c3f09820d
Laura Cabrera <lzcabrera@gmail.com>
2015-01-10 14:35:19 -0800

    add logic display marker only when lat,lng info is available

--- source/javascripts/meetups.js
+++ source/javascripts/meetups.js
@@ -12,28 +12,34 @@
   var generateMarkerData = function(element) {
 
     var groups = element.groups;
+
     groups.forEach( function(element){
 
       var markerIcon = {
-          "url": "/images/meetups/map-pin.png",
+          "url": '/images/meetups/map-pin.png',
           "width": 20,
           "height": 28
       };
       var orgMarkup = "";
-      element.organizers.forEach( function(el){
-        if( typeof el.profileImage == 'undefined'){
-          el.profileImage = "http://photos3.meetupstatic.com/photos/member/d/c/7/0/highres_179096432.jpeg";
-        }
-        orgMarkup += "<div class='organizer'><img src='"+el.profileImage+"' class='profile'><strong>"+el.organizer+"</strong><br>Organizer</div>";
-      });
+      if(element.organizers){
+        element.organizers.forEach( function(el){
+          if( typeof el.profileImage == 'undefined'){
+            el.profileImage = "http://photos3.meetupstatic.com/photos/member/d/c/7/0/highres_179096432.jpeg";
+          }
+          orgMarkup += "<div class='organizer'><img src='"+el.profileImage+"' class='profile'><strong>"+el.organizer+"</strong><br>Organizer</div>";
+        });
+      }
 
       element.infowindow = "<div class='map-marker'><h2>"+element.location+"</h2>"+orgMarkup+"<div class='view'><a href='"+element.url+"' target='_blank'>Go to meetup page</a></div></div>";
       element.picture = markerIcon;
-      markerLocations.push(element);
+
+      if(element.lat && element.lng){
+        markerLocations.push(element);
+      }
     });
   }
 
-  // locations.forEach( generateMarkerData );
+  locations.forEach( generateMarkerData );
 
   handler.buildMap(mapOptions, function() {
     if(navigator.geolocation) {
