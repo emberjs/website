aa90338c4d88ec3382a2455b63e946d095eeb502
Robert Jackson <robert.w.jackson@me.com>
2015-07-13 23:04:09 -0400

    Update for v1.13.4.

--- source/javascripts/app/builds/app.js
+++ source/javascripts/app/builds/app.js
@@ -204,10 +204,10 @@ App.Project.reopenClass({
       projectRepo: 'emberjs/ember.js',
       initialVersion: "1.13.0",
       initialReleaseDate: "2015-06-12",
-      lastRelease: "1.13.3",
-      futureVersion: "1.13.4",
+      lastRelease: "1.13.4",
+      futureVersion: "1.13.5",
       channel: "release",
-      date: "2015-07-06",
+      date: "2015-07-13",
       changelogPath: "CHANGELOG.md",
       enableTestURL: true,
       debugFileName: ".debug.js"
