d2f445afa04d6faf0c78f1ed6d027a94870b95ea
Dave Laird <dlaird@godaddy.com>
2015-09-07 22:29:33 -0700

    updated builds/app.js to remove prototype extensions

--- source/javascripts/app/examples/components/components/gravatar-image.js
+++ source/javascripts/app/examples/components/components/gravatar-image.js
@@ -2,10 +2,10 @@ App.GravatarImageComponent = Ember.Component.extend({
   size: 200,
   email: '',
 
-  gravatarUrl: function() {
+  gravatarUrl: Ember.computed('email', 'size', function() {
     var email = this.get('email'),
         size = this.get('size');
 
     return 'http://www.gravatar.com/avatar/' + md5(email) + '?s=' + size;
-  }.property('email', 'size')
+  })
 });
