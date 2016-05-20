eeae0927c129da9eaa6de721cc0623da1ccff3bc
Ricardo Mendes <rokusu@gmail.com>
2016-05-20 02:54:38 +0100

    Adds Travis CI continuous deployment

---
+++ .travis/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+REPO="git@github.com:emberjs/emberjs.github.com.git"
+
+COMMIT=`git log -1 --oneline`
+git clone $REPO
+rsync -a build/* emberjs.github.com
+cd emberjs.github.com
+git add -A .
+git commit --allow-empty -am "$COMMIT"
+git push origin master
