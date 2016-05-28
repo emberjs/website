7496b8ca7e5847bacd2169cc10f25305f5bd1cbe
David Baker <dbaker@acorncomputersolutions.com>
2016-05-27 21:09:24 -0600

    We'll make sure that we only deploy from master
    Might be cleaner to do this using a deploy line
    per what's discussed here (http://stackoverflow.com/a/34055957)
    but this works for now

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 REPO="git@github.com:emberjs/emberjs.github.com.git"
 
-COMMIT=`git log -1 --oneline`
-git clone $REPO
-rsync -a build/* emberjs.github.com
-cd emberjs.github.com
-git add -A .
-git commit --allow-empty -am "$COMMIT"
-git push origin master
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+  COMMIT=`git log -1 --oneline`
+  git clone $REPO
+  rsync -a build/* emberjs.github.com
+  cd emberjs.github.com
+  git add -A .
+  git commit --allow-empty -am "$COMMIT"
+  git push origin master
+fi
