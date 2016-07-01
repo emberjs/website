86a61a6429d96fea1e05f8a438d3fbe760a13c97
Ricardo Mendes <rokusu@gmail.com>
2016-07-01 13:41:04 +0100

    Prevents publishing for PRs

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
-REPO="git@github.com:emberjs/emberjs.github.com.git"
+set -e
 
-if [ "$TRAVIS_BRANCH" == "master" ]; then
-  COMMIT=`git log -1 --oneline`
-  git clone $REPO
-  rsync -a build/* emberjs.github.com
-  cd emberjs.github.com
-  git add -A .
-  git commit --allow-empty -am "$COMMIT"
-  git push origin master
+REPO="git@github.com:emberjs/emberjs.github.com.git"
+COMMIT=`git log -1 --oneline`
+
+if [[ $TRAVIS_PULL_REQUEST != "false" ]]; then
+  echo "not publishing because this is a pull request."
+  exit 0
 fi
+
+if [[ $TRAVIS_BRANCH != "master" ]]; then
+  echo "not publishing because this is not the master branch."
+  exit 0
+fi
+
+git clone $REPO
+rsync -a build/* emberjs.github.com
+cd emberjs.github.com
+git add -A .
+git commit --allow-empty -am "$COMMIT"
+git push origin master
