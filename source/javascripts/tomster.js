70efaf247d02a8db6ede8b00fc753a0e6cd01b4e
Chris McCuller <cmcculler@salesforce.com>
2016-10-13 23:42:45 -0400

    Add filtering of tomsters

---
+++ source/javascripts/tomster.js
@@ -0,0 +1,28 @@
+$(function() {
+  var filter = getUrlParameter('filter') || 'all';
+  var visibleElements = [];
+  var $this;
+
+  // handle active link
+  $('#tomster-filter a').removeClass('active');
+  if (filter === 'all') {
+    $('#tomster-filter a[href="/tomster"]').addClass('active');
+  } else {
+    $('#tomster-filter a[href$="' + filter + '"]').addClass('active');
+  }
+
+  // filter
+  $('.tomster-wrapper').each(function() {
+    $this = $(this);
+    if($this.attr('data-tags').indexOf(filter) === -1) {
+      $this.hide();
+    }
+  });
+});
+
+function getUrlParameter(name) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    var results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+}
