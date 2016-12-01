9fe5732ca6a3d56b62c30a24efc50c79dc2f8b70
Ben Parnell <benjaminparnell.94@gmail.com>
2016-12-01 20:22:35 +0000

    Fix linking to incorrect version of quick start guide

--- lib/versions.rb
+++ lib/versions.rb
@@ -9,7 +9,9 @@ module Versions
   module Helpers
 
     def current_version
-      @current_version = JSON.parse(open('https://raw.githubusercontent.com/emberjs/guides.emberjs.com/master/snapshots/versions.json').read).last
+      versions = JSON.parse(open('https://raw.githubusercontent.com/emberjs/guides.emberjs.com/master/snapshots/versions.json').read)
+      versions.sort_by! { |version| Gem::Version.new(version[1..-1]) }
+      @current_version = versions.last
     end
   end
 end
