9901b633141b3e0ab721f989aa539f0893baed24
Jessica Jordan <jessica.jordan.fe.dev@gmail.com>
2018-08-31 09:19:23 +0200

    ci(alex): run alex without assuming remote

--- scripts/language-check.sh
+++ scripts/language-check.sh
@@ -1,5 +1,7 @@
-BLOG_HAS_CHANGES=$(git diff --name-status master source/blog/2*)
+git remote add base https://github.com/emberjs/website.git
+git fetch base
+BLOG_HAS_CHANGES=$(git diff --name-status base/master source/blog/2*)
 if [[ $BLOG_HAS_CHANGES ]]
   then
-    alex $(git diff --name-status master source/blog/2* | sed s/^..//)
+    alex $(git diff --name-status base/master source/blog/2* | sed s/^..//)
 fi
